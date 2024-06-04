9cd8cd4f5a35d67ef54357574b33f404918869f8
saleh-f <msalehfarokh@gmail.com>
2024-06-04 19:36:05 +0300

    added 7 segment + ultra sonic counter

--- day9/7segmentUltarSonic/7segmentUltarSonic.ino
+++ day9/7segmentUltarSonic/7segmentUltarSonic.ino
@@ -1,5 +1,9 @@
 // Pin mapping for the segments
 const int segmentPins[] = {2, 3, 4, 5, 6, 7, 8};
+int trig=12;
+int echo= 11;
+int PTT;
+int j=0;
 // Patterns for digits 0-9 (common cathode)
 const int digitPatterns[10][7] = {
     {1, 1, 1, 1, 1, 1, 0}, // 0
@@ -15,6 +19,9 @@ const int digitPatterns[10][7] = {
 };
 
 void setup() {
+  Serial.begin(9600);
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
   // Set segment pins as output
   for (int i = 0; i < 7; i++) {
     pinMode(segmentPins[i], OUTPUT);
@@ -22,11 +29,21 @@ void setup() {
 }
 
 void loop() {
-  for (int i = 0; i <= 9; i++) {
-
-    displayDigit(i);
+  //for (int i = 0; i <= 9; i++) {}
+  digitalWrite(trig,LOW);
+  delayMicroseconds(10);
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig,LOW);
+  PTT=pulseIn(echo,HIGH)* 0.034 / 2;
+  Serial.println(PTT);
+  if(PTT<10){
+    j++;
+    displayDigit(j);
     delay(200); // Wait for 1 second
   }
+  if(j>9)j=0;
+delay(200);
   
  }
 
@@ -35,5 +52,3 @@ void displayDigit(int digit) {
     digitalWrite(segmentPins[i], digitPatterns[digit][i]);
   }
 }
-
-
