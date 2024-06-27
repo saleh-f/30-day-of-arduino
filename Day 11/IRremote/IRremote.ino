27cd4891ffc7e4aa9256ed61d9d7d43928d5a858
saleh-f <msalehfarokh@gmail.com>
2024-06-27 21:51:56 +0300

    corrected Day 12 and some issues

--- Day 11/IRremote/IRremote.ino
+++ Day 11/IRremote/IRremote.ino
@@ -2,75 +2,22 @@
 int IRpin=9;
 IRrecv IR(IRpin);
 decode_results cmd;
-int red=3;
-int green=5;
-int blue=6;
+int ledPinR=3;
+int ledPin=5;
 void setup() {
   Serial.begin(9600);
   IR.enableIRIn();
-  pinMode(red,OUTPUT);
-  pinMode(green,OUTPUT);
-  pinMode(blue,OUTPUT);
+  pinMode(ledPin,OUTPUT);
+  pinMode(ledPinR,OUTPUT);
 }
 
 void loop() {
   while(IR.decode(&cmd)==0){}
   Serial.println(cmd.value,HEX);
-  // if(cmd.value==0xFFA25D)digitalWrite(ledPin,HIGH);
-  // if(cmd.value==0xFF9867)digitalWrite(ledPin,LOW);
-  // if(cmd.value==0xFF629D)digitalWrite(ledPin,HIGH);
-  // if(cmd.value==0xFFE21D)digitalWrite(ledPinR,LOW);
-  if(cmd.value==0xFFA25D){
-  analogWrite(red,255);
-  analogWrite(blue,0);
-  analogWrite(green,0);
-}
-if(cmd.value==0xFF629D){
-  analogWrite(red,0);
-  analogWrite(blue,255);
-  analogWrite(green,0);
-}
-if(cmd.value==0xFFE21D){
-  analogWrite(red,0);
-  analogWrite(blue,0);
-  analogWrite(green,255);
-}
-
-if(cmd.value==0xFF22DD){
-      analogWrite(red,255);
-  analogWrite(green,80);
-  analogWrite(blue,0);
-}
-
-if(cmd.value==0xFF02FD){
-    analogWrite(red,130);
-  analogWrite(blue,170);
-  analogWrite(green,0);
-}
-
-if(cmd.value==0xFFC23D){
-      analogWrite(red,0);
-  analogWrite(green,255);
-  analogWrite(blue,255);
-}
-
-if(cmd.value==0xFFE01F){
-      analogWrite(red,255);
-  analogWrite(green,0);
-  analogWrite(blue,255);
-}
-
-// if(opt.equalsIgnoreCase("lightPink")){
-//       analogWrite(red,200);
-//   analogWrite(green,10);
-//   analogWrite(blue,10);
-// }
-
-if(cmd.value==0xFF9867){
-  analogWrite(red,0);
-  analogWrite(blue,0);
-  analogWrite(green,0);
-}
+  if(cmd.value==0xFE708F)digitalWrite(ledPin,HIGH);
+  if(cmd.value==0xFE58A7)digitalWrite(ledPin,LOW);
+  if(cmd.value==0xFE609F)digitalWrite(ledPinR,HIGH);
+  if(cmd.value==0xFEF00F)digitalWrite(ledPinR,LOW);
   delay(500);
   IR.resume();
 }
